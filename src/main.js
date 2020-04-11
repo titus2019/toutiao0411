5add32ee7a6a23daaa55d4dd5e29af477c538698
titus-w <titus2009@sina.com>
2020-04-11 22:58:45 +0800

    项目初始化

--- src/main.js
+++ src/main.js
@@ -3,11 +3,13 @@ import App from './App.vue'
 import router from './router'
 
 // 引入element
-import Element from '/element-ui'
-
-Vue.use(Element);
+import Element from 'element-ui'
 // 引入样式
 import 'element-ui/lib/theme-chalk/index.css'
+
+import '../styles/index.less'
+
+Vue.use(Element)
 Vue.config.productionTip = false
 
 new Vue({
