82333893754ee5bf1c913bf6101847bc15b1c0d8
titus-w <titus2009@sina.com>
2020-04-11 22:37:01 +0800

    引入element-ui

--- src/main.js
+++ src/main.js
@@ -2,6 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+// 引入element
+import Element from '/element-ui'
+
+Vue.use(Element);
+// 引入样式
+import 'element-ui/lib/theme-chalk/index.css'
 Vue.config.productionTip = false
 
 new Vue({
